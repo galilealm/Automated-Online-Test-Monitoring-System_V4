8e2c56685c2a2e19834f1d8aa033b97713922f90
Fefe29 <felicien.moquet@ensta-bretagne.org>
2025-03-22 17:09:06 +0100

    changement lien direct

--- src/components/btn-handler.jsx
+++ src/components/btn-handler.jsx
@@ -117,7 +117,7 @@ const ButtonHandler = ({ imageRef, cameraRef, videoRef, toggleSession, sessionAc
 
       {/* "Powered by YOLO" and First-time User Information */}
       <div className="footer-info">
-        <p>First use? Check our user guide: <a href="https://fefe29.github.io/Automated-Online-Test-Monitoring-System_V4/user_doc.pdf" target="_blank" rel="noopener noreferrer">User guide</a></p>
+        <p>First use? Check our <a href="https://fefe29.github.io/Automated-Online-Test-Monitoring-System_V4/user_doc.pdf" target="_blank" rel="noopener noreferrer">user guide</a></p>
       </div>
     </div>
   );
